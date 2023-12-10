71ab0704cdb11277ca0cd07c37941df444482c61
Admin <artiomzavatski@gmail.com>
2023-12-10 18:46:29 +0300

    Update parsers. 1 sec win

--- Utils/form/description_parser.py
+++ Utils/form/description_parser.py
@@ -1,28 +1,26 @@
 from bs4 import BeautifulSoup
-import requests
+import aiohttp
+#import requests
 import json
 
-async def description_parser(url : str):
-    
-    # Отправляем запрос на сервер и получаем HTML-страницу
-    response = requests.get(url)
+async def description_parser(url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                html_content = await response.text()
+                soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Проверяем успешность запроса
-    if response.status_code == 200:
-        html_content = response.text
-        soup = BeautifulSoup(html_content, 'html.parser')
+                poster_url = soup.find('meta', property='og:image')['content']
+                title = soup.find('title').text
+                cleaned_title = title.replace("смотреть онлайн — Аниме", "").strip()
 
-        poster_url = soup.find('meta', property='og:image')['content']
-        title = soup.find('title').text
-        cleaned_title = title.replace("смотреть онлайн — Аниме", "").strip()
+                description = soup.find('meta', {'name': 'description'})['content']
+                cleaned_description = description.replace("Смотреть онлайн аниме", "").strip()
 
-        description = soup.find('meta', {'name': 'description'})['content']
-        cleaned_description = description.replace("Смотреть онлайн аниме", "").strip()
+                aggregate_rating_script = soup.find('script', {'type': 'application/ld+json'})
+                rating = None
+                if aggregate_rating_script:
+                    json_data = json.loads(aggregate_rating_script.string)
+                    rating = json_data['aggregateRating']['ratingValue']
 
-        aggregate_rating_script = soup.find('script', {'type': 'application/ld+json'})
-        if aggregate_rating_script:
-            json_data = json.loads(aggregate_rating_script.string)
-            rating = json_data['aggregateRating']['ratingValue']
-
-        # Выводим полученные данные
-        return poster_url, cleaned_title, cleaned_description, rating
+                return poster_url, cleaned_title, cleaned_description, rating
--- Utils/form/parser.py
+++ Utils/form/parser.py
@@ -1,31 +1,29 @@
 from bs4 import BeautifulSoup
-import requests
+#import requests
 import random
+import aiohttp
 
 
-async def parser(url : str, num_links=5):
-    
-    # Отправляем запрос на сервер и получаем HTML-страницу
-    response = requests.get(url)
+async def parser(url: str, num_links=5):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                html_content = await response.text()
+                soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Проверяем успешность запроса
-    if response.status_code == 200:
-        html_content = response.text
-        soup = BeautifulSoup(html_content, 'html.parser')
+                # Находим все элементы с классом 'col-12'
+                items = soup.find_all('div', class_='col-12')
 
-        # Находим все элементы с классом 'col-12'
-        items = soup.find_all('div', class_='col-12')
+                # Создаем список для хранения ссылок
+                href_list = []
 
-        # Создаем список для хранения ссылок
-        href_list = []
+                # Проходим по найденным элементам и извлекаем информацию
+                for item in items:
+                    # Получаем ссылку
+                    link = item.find('a')
+                    if link:
+                        href_list.append(link['href'])
 
-        # Проходим по найденным элементам и извлекаем информацию
-        for item in items:
-            # Получаем ссылку
-            link = item.find('a')
-            if link:
-                href_list.append(link['href'])
-
-        # Выбираем случайные ссылки
-        random_links = random.sample(href_list, min(num_links, len(href_list)))
-        return random_links
+                # Выбираем случайные ссылки
+                random_links = random.sample(href_list, min(num_links, len(href_list)))
+                return random_links
